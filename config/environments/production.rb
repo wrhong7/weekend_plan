63adc8de5c31be8da314dfaf2d774058743ad906
Won Hong <whong@outlook.com>
2015-11-17 09:55:20 -0500

    cache false change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
-  config.assets.debug = true
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::DEBUG
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
