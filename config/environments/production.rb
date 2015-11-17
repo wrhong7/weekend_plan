665a082d781655cf0437cfa48d6d9c214d666ee4
Won Hong <whong@outlook.com>
2015-11-16 21:18:05 -0500

    cache modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,12 @@
 Rails.application.configure do
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
